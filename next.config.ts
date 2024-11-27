139614f6156dbfd051b21bc29b7b8cccd698979f
Felipe Barcelos <felipebarcelospro@Felipes-iMac-Pro.local>
2024-11-27 17:58:04 -0300

    chore: update domains on next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,17 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    remotePatterns: [
-      {
-        hostname: 'images.pexels.com'
-      },
-      {
-        hostname: 'images.unsplash.com'
-      },
-      {
-        hostname: 'github.com'
-      }
-    ]
+    domains: ['*']
   }
 };
 
